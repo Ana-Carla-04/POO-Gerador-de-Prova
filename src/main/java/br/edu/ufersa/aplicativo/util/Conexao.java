ea7b1f9e39c8a8e83b07736b4114438f0085fc04
Ana-Carla-04 <153018029+Ana-Carla-04@users.noreply.github.com>
2026-05-29 20:34:36 -0300

    Add comments for clarity in Conexao.java
    Added comments to clarify the purpose of constants and methods.

--- src/main/java/br/edu/ufersa/aplicativo/util/Conexao.java
+++ src/main/java/br/edu/ufersa/aplicativo/util/Conexao.java
@@ -1,15 +1,16 @@
 package br.edu.ufersa.aplicativo.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection; //Interface que representa a conexão com o banco de dados
+import java.sql.DriverManager; //Classe que gerencia os drivers JDBC e cria conexões
+import java.sql.SQLException; //Exceção específica para erros de banco de dados
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/poo";
-    private static final String usuario = "root";
-    private static final String senha = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/poo"; //É uma constante, URL de conexão com o banco
+    private static final String usuario = "root"; //usuário
+    private static final String senha = "1234"; //senha
 
-    public static Connection abrirConexao() throws SQLException {
-        return DriverManager.getConnection(url, usuario, senha);
+    public static Connection abrirConexao() throws SQLException { //retornar uma conexao tipo Connection, e  pode lançar erros de SQL caso necessário
+        //o DriverManager verifica se o JDBC do MySQL foi carregado,
+        return DriverManager.getConnection(url, usuario, senha); //estabelece conexão com o banco de dados usando url, usuario e senha
     }
 }
